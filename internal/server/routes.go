7cbfbd8ca9d0bbe2502aba2bb93fcf6c9d7a3152
oleksandrkobets <124575789+oleksandrkobets@users.noreply.github.com>
2024-03-01 15:26:55 +0200

    some default handlers added

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -14,6 +16,8 @@ func (s *Server) RegisterRoutes() http.Handler {
 	r.Use(middleware.Logger)
 
 	r.Get("/", s.HelloWorldHandler)
+	r.Get("/another_route/", s.AnotherRouteGetHandler)
+	r.Post("/another_route/", s.AnotherRoutePostHandler)
 
 	return r
 }
@@ -21,6 +25,38 @@ func (s *Server) RegisterRoutes() http.Handler {
 func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	resp := make(map[string]string)
 	resp["message"] = "Hello World"
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) AnotherRouteGetHandler(w http.ResponseWriter, r *http.Request) {
+	resp := make(map[string]string)
+	resp["message"] = "why are you runnin?"
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) AnotherRoutePostHandler(w http.ResponseWriter, r *http.Request) {
+	resp := make(map[string]string)
+	b, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s", b)
+
+	resp["message"] = "Success!"
 
 	jsonResp, err := json.Marshal(resp)
 	if err != nil {
