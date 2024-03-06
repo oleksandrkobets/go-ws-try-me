fcac4af0e74d0200a71aee17a5c0b8d3dd235643
oleksandrkobets <124575789+oleksandrkobets@users.noreply.github.com>
2024-03-06 20:05:32 +0200

    tips from "think differently"

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,7 +3,27 @@ package main
 import (
 	"fmt"
 	"go-ws-try-me/internal/server"
+	"time"
 )
+
+type Book struct {
+	title    string
+	author   string
+	numPages int32
+
+	isSaved bool
+	savedAt time.Time
+}
+
+func (book *Book) SaveBook() {
+	book.isSaved = true
+	book.savedAt = time.Now()
+}
+
+func SaveBook(book *Book) {
+	book.isSaved = true
+	book.savedAt = time.Now()
+}
 
 func main() {
 
